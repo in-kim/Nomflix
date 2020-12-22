f13a191ad8c8525626a45f11833b01c5788c1206
rohaworld <65252214+rohaworld@users.noreply.github.com>
2020-12-22 13:44:10 +0900

    라우터 설정

--- README.md
+++ README.md
@@ -5,4 +5,6 @@ react와 ES6 사용해서 Movie Discoveery App 구축.
 ## Screens
 
 - [ ] Home
-- [ ] TV
+- [ ] TV ㄴShows
+- [ ] Search
+- [ ] Detail
---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default() => (
+    <header>
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/tv">tv</a>
+            </li>
+            <li>
+                <a href="/search">search</a>
+            </li>
+        </ul>
+    </header>
+)
