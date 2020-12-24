ded5a1e33a12a483d914329ab559473402913bb3
rohaworld <65252214+rohaworld@users.noreply.github.com>
2020-12-24 11:21:14 +0900

    api 정의

--- src/api.js
+++ src/api.js
@@ -4,12 +4,38 @@ const api = axios.create({
   baseURL:"https://api.themoviedb.org/3/",
   params:{
     api_key:"27f74db9227ffc474c6211f5414c9834",
-    language:"en-US"
+    language:"ko"
   }
 })
 
+export const MoviesApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
+  movieDetail: (id) => api.get(`movie/${id}`, {
+    params: {
+      append_to_response:'videos'
+    }
+  }),
+  search: (term) => api.get("/search/movie",{
+    params: {
+      query:  encodeURIComponent(term)
+    }
+  }),
+}
 
-// api get은 상대경로로 작성
-api.get("tv/popular");
-
-export default api;
+export const TVApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_tody"),
+  showDetail: (id) => api.get(`tv/${id}`, {
+    params: {
+      append_to_response:'videos'
+    }
+  }),
+  search: term => api.get("search/tv",{
+    params:{
+      query: encodeURIComponent(term)
+    }
+  })
+}
