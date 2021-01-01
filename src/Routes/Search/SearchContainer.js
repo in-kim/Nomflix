00ccbc1d1745d6a30c07efc951bf5641c97d814a
in-kim <ehddls960617@naver.com>
2021-01-01 16:14:17 +0900

    검색 기능 구현, error, not found

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -5,17 +5,34 @@ import SearchPresenter from "./SearchPresenter";
 export default class extends React.Component{
   state = {
     movieResults:null,
-    tvResults:null,
+    showResults:null,
     searchTerm:"",
+    notFound:"",
     error:null,
     loading:false
   };
 
-  handleSubmit = () => {
+  handleSubmit = event => {
+    //reload 막기
+    event.preventDefault();
+    const {target} = event;
+    const { 0 :{defaultValue : inputValue} } = target.children;
     const { searchTerm } = this.state;
+    this.setState({
+      notFound: inputValue,
+    })
     if(searchTerm !== ""){
       this.searchByTerm();
     }
+  }
+
+  updateTerm = event => {
+    const {
+      target: {value}
+    } = event;
+    this.setState({
+      searchTerm:value
+    })
   }
 
   searchByTerm = async () => {
@@ -41,17 +58,20 @@ export default class extends React.Component{
 
   // 객체 비구조화 할당
   render(){
-    const { movieResults, tvResults, searchTerm, error, loading } = this.state;
+    const { movieResults, showResults, searchTerm, notFound, error, loading } = this.state;
     //  epdlxj ghkrdls
     // console.log(this.state);
+    // console.log(notFound);
     return(
       <SearchPresenter 
         movieResults={movieResults}
-        tvResults={tvResults}
+        showResults={showResults}
         searchTerm={searchTerm}
+        notFound={notFound}
         error={error}
         loading={loading}
         handleSubmit={this.handleSubmit}
+        updateTerm={this.updateTerm}
       />
     )
   }
