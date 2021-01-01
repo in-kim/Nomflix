14a91475e48f3edfe167282f3b2379b9ba9f4a72
in-kim <ehddls960617@naver.com>
2021-01-01 16:13:13 +0900

    상단 여백 늘림

--- src/Components/GlobalStyles.js
+++ src/Components/GlobalStyles.js
@@ -13,7 +13,7 @@ const globalStyles = createGlobalStyle`
   body{
     font-family:--apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-size:12px;
-    background-color:rgba(0,0,0,1);
+    background-color:rgba(20, 20, 20, 1);
     color:#fff;
     padding-top:80px;
   }
