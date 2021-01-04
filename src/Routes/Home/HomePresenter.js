fcb5f1669109489b96c8448165d40a6023406dfa
in-kim <ehddls960617@gmail.com>
2021-01-04 18:59:22 +0900

    페이지에 맞는 title 지정

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Helmet from 'react-helmet';
 import Section from "Components/Section";
 import Loader from "Components/Loader";
 import Message from 'Components/Message';
@@ -11,63 +12,68 @@ const Container = styled.div `
 `;
 
 const HomePresenter = ({nowPlaying, upcoming, popular, error, loading}) => 
-    loading ? <Loader /> : ( 
-        <Container>
-            {/* nowPlaying */}
-            {nowPlaying && nowPlaying.length > 0 && (
-                <Section title="Now Playing">
-                    {/* array filter */}
-                    {nowPlaying.map(movie => 
-                        <Poster key={movie.id} 
-                            id={movie.id} 
-                            imageUrl={movie.poster_path} 
-                            title={movie.original_title} 
-                            rating={movie.vote_average}
-                            isMovie={true}
-                            year={movie.release_date && movie.release_date.substring(0,4
-                            )}
-                        />
-                    )}
-                </Section>
-            )}
+    <>
+        <Helmet>
+            <title>Movies | Nomflix</title>
+        </Helmet>
+        {loading ? <Loader /> : ( 
+            <Container>
+                {/* nowPlaying */}
+                {nowPlaying && nowPlaying.length > 0 && (
+                    <Section title="Now Playing">
+                        {/* array filter */}
+                        {nowPlaying.map(movie => 
+                            <Poster key={movie.id} 
+                                id={movie.id} 
+                                imageUrl={movie.poster_path} 
+                                title={movie.original_title} 
+                                rating={movie.vote_average}
+                                isMovie={true}
+                                year={movie.release_date && movie.release_date.substring(0,4
+                                )}
+                            />
+                        )}
+                    </Section>
+                )}
 
-            {/* upcoming */}
-            {upcoming && upcoming.length > 0 && (
-                <Section title="upcoming Movies">
-                    {/* array filter */}
-                    {upcoming.map(movie => 
-                        <Poster key={movie.id} 
-                            id={movie.id} 
-                            imageUrl={movie.poster_path} 
-                            title={movie.original_title} 
-                            rating={movie.vote_average}
-                            isMovie={true}
-                            year={movie.release_date && movie.release_date.substring(0,4)}
-                        />
-                    )}
-                </Section>
-            )}
+                {/* upcoming */}
+                {upcoming && upcoming.length > 0 && (
+                    <Section title="upcoming Movies">
+                        {/* array filter */}
+                        {upcoming.map(movie => 
+                            <Poster key={movie.id} 
+                                id={movie.id} 
+                                imageUrl={movie.poster_path} 
+                                title={movie.original_title} 
+                                rating={movie.vote_average}
+                                isMovie={true}
+                                year={movie.release_date && movie.release_date.substring(0,4)}
+                            />
+                        )}
+                    </Section>
+                )}
 
-            {/* popular */}
-            {popular && popular.length > 0 && (
-                <Section title="popular Movies">
-                    {/* array filter */}
-                    {popular.map(movie => 
-                        <Poster key={movie.id} 
-                            id={movie.id} 
-                            imageUrl={movie.poster_path} 
-                            title={movie.original_title} 
-                            rating={movie.vote_average}
-                            isMovie={true}
-                            year={movie.release_date && movie.release_date.substring(0,4)}
-                        />
-                    )}
-                </Section>
-            )}
+                {/* popular */}
+                {popular && popular.length > 0 && (
+                    <Section title="popular Movies">
+                        {/* array filter */}
+                        {popular.map(movie => 
+                            <Poster key={movie.id} 
+                                id={movie.id} 
+                                imageUrl={movie.poster_path} 
+                                title={movie.original_title} 
+                                rating={movie.vote_average}
+                                isMovie={true}
+                                year={movie.release_date && movie.release_date.substring(0,4)}
+                            />
+                        )}
+                    </Section>
+                )}
 
-            {error && <Message color="#e74c3c" text={error}/>}
-        </Container>
-    );
+                {error && <Message color="#e74c3c" text={error}/>}
+            </Container>
+        )}
+    </>
 
 HomePresenter.propTypes = {
     nowPlaying: PropTypes.array,
--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
+import Helmet from 'react-helmet';
 import Loading from 'Components/Loader';
 import Section from 'Components/Section';
 import Message from 'Components/Message';
@@ -11,53 +12,60 @@ const Container = styled.div`
 `;
 
 const TVPresenter = ({topRated, popular, airingToday, error, loading}) => 
-    loading ? (
-        <Loading/>
-    ) : (
-        <Container>
-            {topRated && topRated.length > 0 &&(
-                <Section title="Top Rated Shows">
-                    {topRated.map(show => 
-                        <Poster key={show.id}
-                            id={show.id}
-                            imageUrl={show.poster_path}
-                            title={show.original_name}
-                            rating={show.vote_average}
-                            year={show.first_air_date && show.first_air_date.substring(0, 4)}
-                        />
-                    )}
-                </Section>
-            )}
-            {popular && popular.length > 0 &&(
-                <Section title="popular Shows">
-                        {popular.map(show => 
-                            <Poster key={show.id}
-                                id={show.id}
-                                imageUrl={show.poster_path}
-                                title={show.original_name}
-                                rating={show.vote_average}
-                                year={show.first_air_date && show.first_air_date.substring(0, 4)}
-                            />
-                        )}
-                </Section>
-            )}
-            {airingToday && airingToday.length > 0 &&(
-                <Section title="Airing Today Shows">
-                        {airingToday.map(show => 
-                            <Poster key={show.id}
-                                id={show.id}
-                                imageUrl={show.poster_path}
-                                title={show.original_name}
-                                rating={show.vote_average}
-                                year={show.first_air_date && show.first_air_date.substring(0, 4)}
-                            />
-                        )}
-                </Section>
-            )}
+    <>
+        <Helmet>
+            <title>TV | Nomflix</title>
+        </Helmet>
+        {
 
-            {error && <Message color="#e74c3c" text={error} />}
-        </Container>
-    )
+        loading ? (
+            <Loading/>
+        ) : (
+            <Container>
+                {topRated && topRated.length > 0 &&(
+                    <Section title="Top Rated Shows">
+                        {topRated.map(show => 
+                            <Poster key={show.id}
+                                id={show.id}
+                                imageUrl={show.poster_path}
+                                title={show.original_name}
+                                rating={show.vote_average}
+                                year={show.first_air_date && show.first_air_date.substring(0, 4)}
+                            />
+                        )}
+                    </Section>
+                )}
+                {popular && popular.length > 0 &&(
+                    <Section title="popular Shows">
+                            {popular.map(show => 
+                                <Poster key={show.id}
+                                    id={show.id}
+                                    imageUrl={show.poster_path}
+                                    title={show.original_name}
+                                    rating={show.vote_average}
+                                    year={show.first_air_date && show.first_air_date.substring(0, 4)}
+                                />
+                            )}
+                    </Section>
+                )}
+                {airingToday && airingToday.length > 0 &&(
+                    <Section title="Airing Today Shows">
+                            {airingToday.map(show => 
+                                <Poster key={show.id}
+                                    id={show.id}
+                                    imageUrl={show.poster_path}
+                                    title={show.original_name}
+                                    rating={show.vote_average}
+                                    year={show.first_air_date && show.first_air_date.substring(0, 4)}
+                                />
+                            )}
+                    </Section>
+                )}
+
+                {error && <Message color="#e74c3c" text={error} />}
+            </Container>
+        )}
+    </>
 
 TVPresenter.propTypes = {
     topRated: PropTypes.array,
